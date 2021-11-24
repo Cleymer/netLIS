32a1c8645bfc1e7b2d828072c1ee0b12f796fb3c
cleymer <cleymer.zavala8807@est.uca.edu.ni>
2021-11-24 14:32:32 -0600

    Agregar tipo orden

---
+++ Aplicacion/TipoOrden/Consulta.cs
@@ -0,0 +1,34 @@
+﻿using Dominio.Model;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.TipoOrden
+{
+    public class Consulta
+    {
+
+        public class Ejecuta : IRequest<List<TblCatTipoOrden>> { }
+
+        public class Manejador : IRequestHandler<Ejecuta, List<TblCatTipoOrden>>
+        {
+
+            private readonly netLisContext _context;
+            public Manejador(netLisContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<TblCatTipoOrden>> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var tipoOrden = await _context.TblCatTipoOrdens.ToListAsync();
+                return tipoOrden;
+            }
+        }
+    }
+}
---
+++ Aplicacion/TipoOrden/ConsultaId.cs
@@ -0,0 +1,42 @@
+﻿using Dominio.Model;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.TipoOrden
+{
+    public class ConsultaId
+    {
+
+        public class TipoOrdenUnico : IRequest<TblCatTipoOrden>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<TipoOrdenUnico, TblCatTipoOrden>
+        {
+
+            private readonly netLisContext _context;
+            public Manejador(netLisContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<TblCatTipoOrden> Handle(TipoOrdenUnico request, CancellationToken cancellationToken)
+            {
+                var tipoOrden = await _context.TblCatTipoOrdens.FindAsync(request.Id);
+                /*if (tipoOrden == null)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.NotFound, new { mensaje = "El curso no existe" });
+                }*/
+
+                return tipoOrden;
+            }
+        }
+
+    }
+}
