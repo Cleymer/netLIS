d3210e14390698847da4a1840b97ceb44f5a4b8b
cleymer <cleymer.zavala8807@est.uca.edu.ni>
2021-11-25 12:31:56 -0600

    Cambios variados

---
+++ Aplicacion/Orden/Consulta.cs
@@ -0,0 +1,33 @@
+﻿using Dominio.Model;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Orden
+{
+    public class Consulta
+    {
+
+        public class Ejecuta : IRequest<List<TblOrdenes>> { }
+
+        public class Manejador : IRequestHandler<Ejecuta, List<TblOrdenes>>
+        {
+            private readonly netLisContext _context;
+            public Manejador(netLisContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<TblOrdenes>> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var orden = await _context.TblOrdenes.ToListAsync();
+                return orden;
+            }
+        }
+    }
+}
---
+++ Aplicacion/OrdenDetalle/Consulta.cs
@@ -0,0 +1,33 @@
+﻿using Dominio.Model;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.OrdenDetalle
+{
+    public class Consulta
+    {
+        public class Ejecuta : IRequest<List<TblOrdenesDetalle>> { }
+
+
+        public class Manejador : IRequestHandler<Ejecuta, List<TblOrdenesDetalle>>
+        {
+            private readonly netLisContext _context;
+            public Manejador(netLisContext context)
+            {
+                _context = context;
+            }
+            public async Task<List<TblOrdenesDetalle>> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var ordenDetalle = await _context.TblOrdenesDetalles.ToListAsync();
+                return ordenDetalle;
+
+            }
+        }
+    }
+}
