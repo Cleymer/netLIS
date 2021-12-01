971f868d21b1b195780cca9288163970d907bb79
DMarcelo13 <david.saenz8818@est.uca.edu.ni>
2021-12-01 17:58:47 -0600

    Tabla-Perfiles

---
+++ WebAPI/Controllers/PerfilController.cs
@@ -0,0 +1,23 @@
+﻿using Dominio.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aplicacion.Perfiles;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class PerfilController : MiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<TblCatPerfiles>>> Get()
+        {
+            return await Mediator.Send(new Consulta.Ejecuta());
+        }
+
+    }
+}
